4fa31920a296a4f929145335774456d8ec1842c0
Kacper Weron <weron.kacper@gmail.com>
2022-12-05 20:47:47 +0100

    innitial commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+console.log("Hello World");
+
+let button = document.querySelector(".js-object__button");
+let object = document.querySelector(".js-object");
+
+
+button.addEventListener("click", () => {
+  
+   object.innerText = (object.innerText === "🍕")
+   ? "🍽️" : (object.innerText = "🍕");
+
+   button.innerText = (button.innerText === "Zjedz kawałek") 
+   ? "Zrób nowy" : (button.innerText = "Zjedz kawałek");
+
+});
+
